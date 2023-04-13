3e14dddbf1eaeb2ea9426b3e4b221290d40a2058
QGritFitness <130677428+QGritFitness@users.noreply.github.com>
2023-04-13 11:10:22 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// gym.js
+// document.addEventListener('DOMContentLoaded', () => {
+//    const navLinks = document.querySelectorAll('header nav ul li a');
+
+//    navLinks.forEach(link => {
+//        link.addEventListener('click', (event) => {
+//            event.preventDefault();
+//            alert(`Navigating to ${link.textContent}...`);
+//        });    });      });
